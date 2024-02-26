f84a7701eeef7d19163906945b6777d09b2ce795
Dean3035 <dean27vip30@outlook.com>
2024-02-26 13:13:24 +0800

    测试脚本上传
    测试脚本上传
    SurvivalCraftGameMode

--- Source/SurvivalCraft/SurvivalCraftGameMode.cpp
+++ Source/SurvivalCraft/SurvivalCraftGameMode.cpp
@@ -11,4 +11,5 @@ ASurvivalCraftGameMode::ASurvivalCraftGameMode()
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
+	UE_LOG(LogTemp, Log, TEXT("Test"));
 }
